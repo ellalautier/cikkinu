6d17ed22986a050df33e786946607bfbbec54691
Ella <ellalautier@gmail.com>
2017-02-28 13:18:20 +0100

    addChickens method updated

--- tutorials-scm/src/main/java/com/examples/one/petstore/ChickenDB.java
+++ tutorials-scm/src/main/java/com/examples/one/petstore/ChickenDB.java
@@ -1,14 +1,22 @@
 import java.util.*;
 public class ChickenDB{
 	
+	
 	ArrayList<Chicken> chickens = new ArrayList<>();
 	
 	boolean addChicken(Chicken c){
+		if(chickens.size()<10){
+			chicken.add(c);
+			return true;
+		}else {
+			System.out.println("Error: too many chickens");
+			return false;
+		}
 		
 	}
 	
 	boolean delChicken(int id){
-	
+		
 	}
 
 
