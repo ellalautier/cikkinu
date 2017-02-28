a75a33451139fa70a87b21e69f47adcbf44c29d0
Miguel Dingli <dingli.miguel.15@um.edu.mt>
2017-02-28 13:24:39 +0100

    Added Cikkinu.java properties and methods

--- tutorials-scm/src/main/java/com/examples/one/petstore/Chicken.java
+++ tutorials-scm/src/main/java/com/examples/one/petstore/Chicken.java
@@ -1,5 +1,64 @@
 package com.examples.one.petstore;
 
 public class Chicken {
-    
+
+    private int id;
+    private String name, breed;
+    private boolean loudCluck;
+    private int exerciseRequiredPerDay;
+    private boolean friendlyWithChildren;
+
+    public Chicken(int id, String name, String breed) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public boolean isLoudCluck() {
+        return loudCluck;
+    }
+
+    public void setLoudCluck(boolean loudCluck) {
+        this.loudCluck = loudCluck;
+    }
+
+    public int getExerciseRequiredPerDay() {
+        return exerciseRequiredPerDay;
+    }
+
+    public void setExerciseRequiredPerDay(int exerciseRequiredPerDay) {
+        this.exerciseRequiredPerDay = exerciseRequiredPerDay;
+    }
+
+    public boolean isFriendlyWithChildren() {
+        return friendlyWithChildren;
+    }
+
+    public void setFriendlyWithChildren(boolean friendlyWithChildren) {
+        this.friendlyWithChildren = friendlyWithChildren;
+    }
 }
