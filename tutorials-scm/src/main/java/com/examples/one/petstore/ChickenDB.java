9d1f36e66f1c4db832396b393faecde6ec791c0b
Miguel Dingli <dingli.miguel.15@um.edu.mt>
2017-02-28 15:50:21 +0100

    Added ChickenDB tests

--- tutorials-scm/src/main/java/com/examples/one/petstore/ChickenDB.java
+++ tutorials-scm/src/main/java/com/examples/one/petstore/ChickenDB.java
@@ -1,36 +1,44 @@
 package com.examples.one.petstore;
+
+import com.examples.one.petstore.exceptions.ChickenNotFoundException;
+
 import java.util.*;
-public class ChickenDB{
-	
-	ArrayList<Chicken> chickens = new ArrayList<Chicken>();
-	
-	public boolean addChicken(final Chicken c){
-		if(chickens.size() < 10){
-			chickens.add(c);
-			return true;
-		}else {
-			System.out.println("Error: too many chickens");
-			return false;
-		}
-		
-	}
 
-    public boolean delChicken(final int id){
-		if(chickens.size() >= id){
-			chickens.remove(id);
-			return true;
-		}else{
-			System.out.println("Error 404: chicken not found");
-			return false;
-		}
-	}
+public class ChickenDB {
 
-    public Chicken getChicken(int id){
-		return chickens.get(id);
-	}
+    ArrayList<Chicken> chickens = new ArrayList<Chicken>();
 
-    public int countChickens(){
-		return chickens.size();
-	}
+    public boolean addChicken(final Chicken c) {
+
+        if (chickens.size() < 10) {
+            chickens.add(c);
+            return true;
+        } else {
+            System.out.println("Error: too many chickens!");
+            return false;
+        }
+
+    }
+
+    public boolean delChicken(final int id) {
+
+        final int previousSize = chickens.size();
+        chickens.removeIf(chick -> chick.getId() == id);
+        return chickens.size() < previousSize;
+    }
+
+    public Chicken getChicken(int id) throws ChickenNotFoundException {
+
+        for (final Chicken chick : chickens) {
+            if (chick.getId() == id) {
+                return chick;
+            }
+        }
+        throw new ChickenNotFoundException("Chicken with ID " + id + " not found!");
+    }
+
+    public int countChickens() {
+        return chickens.size();
+    }
 }
 
---
+++ tutorials-scm/src/main/java/com/examples/one/petstore/exceptions/ChickenNotFoundException.java
@@ -0,0 +1,8 @@
+package com.examples.one.petstore.exceptions;
+
+public class ChickenNotFoundException extends Exception {
+
+    public ChickenNotFoundException(String message) {
+        super(message);
+    }
+}
