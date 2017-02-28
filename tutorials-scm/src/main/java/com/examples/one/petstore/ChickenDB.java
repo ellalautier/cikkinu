c3ff60501a6de428b2d4ba92ef3b2ce06242cca0
Ella <ellalautier@gmail.com>
2017-02-28 13:11:32 +0100

    adding ChickenDB file

---
+++ tutorials-scm/src/main/java/com/examples/one/petstore/ChickenDB.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class ChickenDB{
+	
+	ArrayList<Chicken> chickens = new ArrayList<>();
+	
+	boolean addChicken(Chicken c){
+		
+	}
+	
+	boolean delChicken(int id){
+	
+	}
+
+
+	Chicken getChicken(int id){
+	
+	}
+	
+	
+	int countChickens(){
+	
+	
+	}
+
+
+
+
+}
