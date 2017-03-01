5761c5ce34c8f4fa65cb0326a789b2c1991dfdc1
Miguel Dingli <dingli.miguel.15@um.edu.mt>
2017-03-01 21:34:47 +0100

    Back to square 1

--- tutorials-scm/src/main/java/com/examples/one/petstore/exceptions/ChickenNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.examples.one.petstore.exceptions;
-
-public class ChickenNotFoundException extends Exception {
-
-    public ChickenNotFoundException(String message) {
-        super(message);
-    }
-}
